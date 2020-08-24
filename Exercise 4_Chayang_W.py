aebc62e82ed8dfff0fb638421818374b3e325bea
chayangw <chayangw@gmail.com>
2020-08-24 22:03:48 +0700

    Assignment Finished

---
+++ Exercise 4_Chayang_W.py
@@ -0,0 +1,11 @@
+fndeng = "Foundation English"
+gb = "General Business"
+introcomsys = "Introduction to Computer Systems"
+comprogram = "Computer Programming"
+
+print("----------Your Score----------")
+print(fndeng, "90")
+print(gb, "75")
+print(introcomsys, "86")
+print(comprogram, "67")
+print("----------Thank You-----------")
